33ab747684216c7ddb24877b1f27ebcef0a4a346
Igor Klepuszewski <igor.klepuszewski@gmail.com>
2024-09-06 17:27:10 +0200

    establish the api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from django.urls import include, path
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
